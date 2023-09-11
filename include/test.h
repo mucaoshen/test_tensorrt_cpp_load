5781ef5fecdedddbb07bb6d60a4d761421717e75
Jack Huang <huang176368745@gmail.com>
2023-09-11 15:12:32 +0800

    : first submit

---
+++ include/test.h
@@ -0,0 +1,49 @@
+#include <NvInfer.h>
+#include <string>
+#include <vector>
+#include <memory>
+
+#define CHECK(call, resContent) check(call, __LINE__, __FILE__, resContent)
+
+inline bool check(cudaError_t e, int iLine, const char *szFile, std::string& resContent) {
+	if (e != cudaSuccess) {
+		resContent = "CUDA runtime API error ";
+		resContent += std::string(cudaGetErrorName(e));
+		resContent += " at line " + std::to_string(iLine);
+		resContent += " in file " + std::string(szFile);
+		resContent += "\n";
+		// std::cout << "CUDA runtime API error " << cudaGetErrorName(e) << " at line " << iLine << " in file " << szFile << std::endl;
+        return false;
+	}
+	resContent = "";
+	return true;
+};
+
+class TRTLogger: public nvinfer1::ILogger {
+public:
+	nvinfer1::ILogger::Severity reportableServerity;
+
+public:
+	TRTLogger(nvinfer1::ILogger::Severity severity = nvinfer1::ILogger::Severity::kVERBOSE): reportableServerity(severity) {
+	}
+	void log(nvinfer1::ILogger::Severity severity, const char* msg) noexcept override;
+
+};
+
+class B {
+	public:
+	virtual int modelLoad(const std::string& m_modelPath) = 0;
+
+};
+
+class A: public B {
+	public:
+	int modelLoad(const std::string& m_modelPath) override;
+	static TRTLogger s_Logger;
+	private:
+	nvinfer1::ICudaEngine* m_engine;
+};
+
+int bytesToInteger(char* buffer) {
+	return *reinterpret_cast<int*>(buffer);
+}
---
+++ src/test.cpp
@@ -0,0 +1,85 @@
+#include "test.h"
+#include <iostream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+
+void TRTLogger::log(nvinfer1::ILogger::Severity severity, const char* msg) noexcept {
+	if (severity > reportableServerity) {
+		return;
+	}
+	switch (severity)
+	{
+	case nvinfer1::ILogger::Severity::kINTERNAL_ERROR:
+		std::cout<<"INTERNAL_ERROR: " + std::string(msg)<<std::endl;
+		break;
+
+	case nvinfer1::ILogger::Severity::kERROR:
+		std::cout<<"ERROR: " + std::string(msg)<<std::endl;
+		break;
+
+	case nvinfer1::ILogger::Severity::kWARNING:
+		std::cout<<"WARNING: " + std::string(msg)<<std::endl;
+		break;
+
+	case nvinfer1::ILogger::Severity::kINFO:
+		std::cout<<"INFO: " + std::string(msg)<<std::endl;
+		break;
+	
+	default:
+		std::cout<<"VERBOSE: " + std::string(msg)<<std::endl;
+		break;
+	}
+}
+
+TRTLogger A::s_Logger = TRTLogger();
+int A::modelLoad(const std::string& m_modelPath) {
+	std::string tmpLogStr;
+	bool isSuccess = CHECK(cudaSetDevice(0), tmpLogStr);
+	if (!isSuccess) {
+		throw std::runtime_error("cuda set device in modelLoad unsuccessfully : " + tmpLogStr);
+	}
+
+	std::ifstream engineFile(m_modelPath, std::ios::binary);
+	long int fsize = 0;
+	// get file size
+	std::cout<<"Parsing model file!"<<std::endl;
+	engineFile.seekg(0, engineFile.end);
+	fsize = engineFile.tellg();
+	engineFile.seekg(0, engineFile.beg);
+	// get meta info
+	char* metaLenBytes;
+	metaLenBytes = (char*)malloc(4);
+	engineFile.read(metaLenBytes, 4);
+	int metaLen = bytesToInteger(metaLenBytes);
+	if (metaLenBytes != nullptr) free(metaLenBytes);
+	// TODO: get meta json str
+	engineFile.seekg(4, engineFile.beg);
+	char* metaBytes;
+	metaBytes = (char*)malloc(metaLen);
+	engineFile.read(metaBytes, metaLen);
+	if (metaBytes != nullptr) free(metaBytes);
+	// get model info
+	std::vector<char> engineStr(fsize - metaLen - 4);
+	engineFile.seekg(metaLen + 4, engineFile.beg);
+	engineFile.read(engineStr.data(), fsize - metaLen - 4);
+
+	if (engineStr.size() == 0) {
+		std::cout<<"Failed getting serialized engine!"<<std::endl;
+		engineFile.close();
+		return -1;
+	}
+	engineFile.close();
+	std::cout<<"Succeeded getting serialized engine!"<<std::endl;
+
+	// create inference env, deserialize engine
+	nvinfer1::IRuntime* m_runtime {nvinfer1::createInferRuntime(s_Logger)};
+	m_engine = m_runtime->deserializeCudaEngine(engineStr.data(), engineStr.size());
+	if (m_engine == nullptr) {
+		std::cout<<"Failed loading engine!"<<std::endl;
+		return -1;
+	}
+	return 0;
+}
